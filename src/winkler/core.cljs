8ba4e05469e37ab85c4f6cf713d525e6a52752ff
Nathan Donolli <ndonolli@gmail.com>
2020-07-30 02:41:46 -0500

    change generator signature

--- src/winkler/core.cljs
+++ src/winkler/core.cljs
@@ -2,14 +2,33 @@
   (:require [winkler.entropy :refer [DEFAULT collect-entropy]]))
 
 (defn generate
-  "Returns a sequence of integers with at least n bits of entropy, as provided by the bit-limit. Takes an optional argument for a max-bit per delta in the sequnce. Defaults to 4"
-  ([bit-limit] (generate bit-limit (:max-bits DEFAULT)))
-  ([bit-limit max-bits]
-   (->> (collect-entropy max-bits)
-        (reductions (fn [[_ harvested] [delta _ entropy]]
-                      [delta (+ harvested entropy)])
-                    [0 0])
-        (take-while (fn [[_ harvested]]
-                      (>= (+ bit-limit (* 2 max-bits)) harvested)))
-        (map first)
-        (rest))))
+  "Returns a lazy sequence of integers with increasing bits of entropy. Can optionally provide additional options in the form:
+
+   - `:entropy` - takes from the sequence until the combined entropy is at least the given amount.
+   - `:max-bits` - the max entropy value allowed per generation (default 4).
+
+   ```clojure
+   ;; Generate random integers with at least 100 bits of combined entropy
+   (generate :entropy 100) ;; => (1134 -419 16631 -2872 ...)
+
+   ;; Without any arguments, `generate` will produce infinitely. So take precautions:
+   (take 3 (generate)) ;; => (5081 -1092 -4678)
+   ;; Although lazy, each take does require running timed computations in order to calculate entropy values.
+   ```
+   "
+  ([& {:keys [entropy max-bits]
+       :or {entropy nil
+            max-bits (:max-bits DEFAULT)}}]
+   (cond->> (collect-entropy max-bits)
+     true
+     (reductions (fn [[_ harvested] [delta _ entropy]]
+                   [delta (+ harvested entropy)])
+                 [0 0])
+     entropy
+     (take-while (fn [[_ harvested]]
+                   (>= (+ entropy (* 2 max-bits)) harvested)))
+     true (map first)
+     true (rest))))
+
+(comment
+  (take 9 (generate)))
